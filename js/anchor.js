18a6a63a38922daa7c7d3e37a62fb845990e4e5b
romanich141 <romaovcharenko18011993@gmail.com>
2018-08-27 15:55:25 +0300

    first commit

---
+++ js/anchor.js
@@ -0,0 +1,5 @@
+$('a[href^="#"]').click(function(){
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+    return false;
+    });
---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    arrows:false,
+    dots:true,
+    speed: 800,
+    slidesToScroll: 1,
+
+  });
